c9eedee10386d8b8a9579e74ebfc5ce18e92b7f1
kehindeBankole <bankolek1@gmail.com>
2024-02-06 21:20:11 +0100

    Initial Commit

---
+++ travelapp/travelappApp.swift
@@ -0,0 +1,17 @@
+//
+//  travelappApp.swift
+//  travelapp
+//
+//  Created by kehinde on 06/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct travelappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
